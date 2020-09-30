835abec93efb648215e23f695cfe8af5b1b8a777
gryzun33 <yakushevao777@gmail.com>
2020-09-30 13:30:33 +0300

    feat: add three functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+    if(!arr || arr.length === 0) {
+        return 0;
+    }
+    let min = arr[0];
+    for(let i = 1; i < arr.length; i++) {
+        min = Math.min(min, arr[i]);
+    }
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+    if(!arr || arr.length === 0) {
+        return 0;
+    }
+  let max = Math.max(...arr);
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+    if(!arr || arr.length === 0) {
+        return 0;
+    }
+  let sum = arr.reduce((s, elem) => s + elem, 0);
+  let average = sum/arr.length;
+  return average;
 }
